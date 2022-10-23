94253499b6fc46211f5bd2433f57c2a997068283
Aleksandar Zeljkovic <aleksandar.zeljkovic91@gmail.com>
2022-10-23 21:31:59 +0200

    Header styling added.

--- reactSite/components/Header.jsx
+++ reactSite/components/Header.jsx
@@ -9,9 +9,8 @@ const Header = () => {
              alt="react-logo"
         />
         <ul className="nav-items">
-          <li>Pricing</li>
-          <li>About</li>
-          <li>Contact</li>
+          <li className="title">ReactFacts</li>
+          <li className="subtitle">React Course - Project 1</li>
         </ul>
       </nav>
     </header>
