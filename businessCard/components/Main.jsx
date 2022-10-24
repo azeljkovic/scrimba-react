4c4b8e85f1be9a9c4b62223be1cc60563f2e87dd
Aleksandar Zeljkovic <aleksandar.zeljkovic91@gmail.com>
2022-10-24 21:33:17 +0200

    Added business card artifacts.

---
+++ businessCard/components/Main.jsx
@@ -0,0 +1,11 @@
+import image from "../images/image.png";
+
+const Main = () => {
+  return (
+  <div className="card">
+    <img src={image}  alt="my-image"/>
+  </div>
+  );
+}
+
+export default Main;
