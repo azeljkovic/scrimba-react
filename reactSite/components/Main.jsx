33514de00d967198805b0674147230bf45c98472
Aleksandar Zeljkovic <aleksandar.zeljkovic91@gmail.com>
2022-10-24 20:39:46 +0200

    Added final styling.

--- reactSite/components/Main.jsx
+++ reactSite/components/Main.jsx
@@ -1,11 +1,11 @@
 const Main = () => {
   return (
-    <nav className="body">
+    <nav className="main">
       <h1>Fun facts about React</h1>
       <ul>
         <li>Was first released in 2013</li>
         <li>Was originally created by Jordan Walke</li>
-        <li>Has well over 100k stars on Github</li>
+        <li>Has well over 100k stars on GitHub</li>
         <li>Is maintained by Facebook</li>
         <li>Powers thousands of enterprise apps, including mobile apps</li>
       </ul>
