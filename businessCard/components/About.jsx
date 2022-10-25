ff086ceb88b14990f384d09f712fddfb1ea17ff5
Aleksandar Zeljkovic <aleksandar.zeljkovic91@gmail.com>
2022-10-25 14:04:47 +0200

    Added About component.

---
+++ businessCard/components/About.jsx
@@ -0,0 +1,16 @@
+const About = () => {
+  return (
+    <div className="about">
+      <h1>About</h1>
+      <div>I am a frontend developer with a particular interest in making things simple and automating daily tasks. I
+        try to keep up with security and best practices, and am always looking for new things to learn.
+      </div>
+      <h1>Interests</h1>
+      <div>Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture
+        ninja. Coffee fanatic.
+      </div>
+    </div>
+  );
+}
+
+export default About;
