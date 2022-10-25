927f2f4bc35972177eb2ce57ff99d64ce9af56f7
Aleksandar Zeljkovic <aleksandar.zeljkovic91@gmail.com>
2022-10-25 13:51:30 +0200

    Buttons added.

--- businessCard/components/Info.jsx
+++ businessCard/components/Info.jsx
@@ -1,13 +1,25 @@
 import image from "../images/image.png";
+import emailIcon from "../images/email-icon.png"
+import linkedinIcon from "../images/linkedin-icon.png"
 
 const Info = () => {
   return (
-  <div>
-    <img src={image}  alt="my-image"/>
-    <h1 className="name">Laura Smith</h1>
-    <h2 className="title">Frontend Developer</h2>
-    <h3 className="website">laurasmith.website</h3>
-  </div>
+    <div>
+      <img src={image} alt="my-image"/>
+      <h1 className="name">Laura Smith</h1>
+      <h2 className="title">Frontend Developer</h2>
+      <h3 className="website">laurasmith.website</h3>
+      <div className="buttons">
+        <button className="email-button">
+          <img src={emailIcon} alt="emailIcon"/>
+          Email
+        </button>
+        <button className="linkedin-button">
+          <img src={linkedinIcon} alt="linkedinIcon"/>
+          LinkedIn
+        </button>
+      </div>
+    </div>
   );
 }
 
